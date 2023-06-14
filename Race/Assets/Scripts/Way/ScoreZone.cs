2efe4ca06ca1086f940c09ad78af2bde9eaae933
Viacheslav Born <64132803+TABASCO1990@users.noreply.github.com>
2023-06-14 13:54:13 +0500

    Update Obstacle

--- Race/Assets/Scripts/Way/ScoreZone.cs
+++ Race/Assets/Scripts/Way/ScoreZone.cs
@@ -2,4 +2,22 @@ using UnityEngine;
 
 public class ScoreZone : MonoBehaviour
 {
+    private void OnEnable()
+    {
+        foreach (Transform obstacle in transform)
+        {
+            ActivateObstacle(obstacle);
+        }
+    }
+
+    private void ActivateObstacle(Transform obstacle)
+    {
+        if (obstacle.TryGetComponent(out Enemy enemyCar))
+        {
+            if (enemyCar.gameObject.activeSelf == false)
+            {
+                enemyCar.gameObject.SetActive(true);
+            }
+        }
+    }
 }
